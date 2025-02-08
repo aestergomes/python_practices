0f399bfeddff78acc9d9c47db4b1eef999d256c6
Ester <estergomess439@fmail.com>
2025-02-08 20:12:55 -0300

    terceiro commit

--- methods.py
+++ methods.py
@@ -1,9 +1,15 @@
 # determine length, index of first occurrence of 'and', index of last occurrence of 'you', and count of 'you' in the verse string.
 
 verse = "If you can keep your head when all about you\n  Are losing theirs and blaming it on you,\nIf you can trust yourself when all men doubt you,\n  But make allowance for their doubting too;\nIf you can wait and not be tired by waiting,\n  Or being lied about, don’t deal in lies,\nOr being hated, don’t give way to hating,\n  And yet don’t look too good, nor talk too wise:"
-print(verse)
+print(verse, "\n")
 
-print("comprimento da lista:", len(verse))
-print("primeira ocorrencia de 'and:'", verse.find('and'))
-print("última ocorrencia de 'you:'", verse.rfind('you'))
-print("quantidade de 'you:'", verse.count('you'))
+message = "Verse has a length of {} characters.\nThe first occurence of the \
+word 'and' occurs at the {}th index.\nThe last occurence of the word 'you' \
+occurs at the {}th index.\nThe word 'you' occurs {} times in the verse."
+
+length = len(verse)
+first_idx = verse.find('and')
+last_idx = verse.rfind('you')
+count = verse.count('you')
+
+print(message.format(length, first_idx, last_idx, count))
